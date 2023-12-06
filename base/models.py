aee95e0b4b53f1d263a34e6f942653a7dd187839
sardor <sshavkatov900@gmail.com>
2023-12-06 21:53:07 +0500

    added base models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuModel(models.Model):
+    title = models.CharField('Name',
+                             max_length=50)
+    description = models.TextField('name',
+                                   max_length=250)
+    price = models.PositiveIntegerField('price($)')
+    image = models.ImageField('image')
+    specials = models.BooleanField('today special')
+
+    class Meta:
+        verbose_name = 'Meal'
+        verbose_name_plural = 'Menu'
+
+
+class Reservation(models.Model):
+    date_time = models.DateTimeField('date and time')
+    name = models.CharField('client name',
+                            max_length=50)
+    email = models.EmailField('email')
+    gest = models.PositiveSmallIntegerField('guests number')
+    phone = models.PositiveBigIntegerField('phone number')
+
+    class Meta:
+        verbose_name = 'Client'
+        verbose_name_plural = 'Orders'
+
+
+class Teams(models.Model):
+    name = models.CharField('name',
+                            max_length=50)
+    job_title = models.CharField('job title',
+                                 max_length=50)
+    image = models.ImageField()
+
+
+class Gallery(models.Model):
+    image = models.ImageField()
