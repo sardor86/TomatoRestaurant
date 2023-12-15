d50c96acb0061330d76b68dbc2af4790c84171a5
sardor <sshavkatov900@gmail.com>
2023-12-15 21:47:06 +0500

    added users model

--- user/views.py
+++ user/views.py
@@ -3,5 +3,9 @@ from django.core.handlers.wsgi import WSGIRequest
 
 
 def register_login(request: WSGIRequest):
+    contex: dict = {
+        'title': 'account'
+    }
     return render(request,
-                  'user/templates/shop_account.html')
+                  'user/templates/shop_account.html',
+                  contex)
