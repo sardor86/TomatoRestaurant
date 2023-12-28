ccfc60e8b92b194fa0fbc74bed2dec9380015780
sardor <sshavkatov900@gmail.com>
2023-12-28 22:39:51 +0500

    created form for login

--- user/forms.py
+++ user/forms.py
@@ -19,6 +19,9 @@ class UserRegisterForm(forms.Form):
         if data['password1'] != data['password2']:
             return 'the passwords are not similar'
 
+        if not (UserModel.objects.filter(email=data['email']).first() is None):
+            return 'This email is already registered'
+
         return True
 
     def save(self):
@@ -32,3 +35,27 @@ class UserRegisterForm(forms.Form):
                          password=password_hash,
                          salt=salt)
         user.save()
+
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def check(self):
+        data = self.cleaned_data
+        if len(data['password1']) < 8:
+            return 'password length is too short'
+
+        user = UserModel.objects.filter(email=data['email']).first()
+        if user is None:
+            return 'this is not registered'
+
+        password_hash = hashlib.pbkdf2_hmac('sha256',
+                                            data['password'].encode('utf-8'),
+                                            user.salt,
+                                            100000)
+
+        if password_hash != data['password']:
+            return 'password is incorrect'
+
+        return True
