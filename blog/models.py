231bdcfe10e06b664b12e492fd5e651dcbef6c7d
sardor <sshavkatov900@gmail.com>
2024-01-11 21:51:57 +0500

    added blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    image = models.ImageField(upload_to='blog')
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=1500)
+
+    class Meta:
+        db_table = 'blog'
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self) -> str:
+        return f'Blog<{self.title}>'
+
+    def __repr__(self) -> str:
+        return f'Blog<{self.title}>'
