e7c5426d7d3fc4d2984a22dbe557964f74ec92ad
sardor <sshavkatov900@gmail.com>
2024-01-11 22:23:04 +0500

    added admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Blog
 
+
+@admin.register(Blog)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
