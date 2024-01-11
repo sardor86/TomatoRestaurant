7872b9e8efaa81de798e254997cc8b4bc8215380
sardor <sshavkatov900@gmail.com>
2024-01-11 22:23:27 +0500

    updated main blog template

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,13 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Blog
+
 
 def blog(request: WSGIRequest) -> HttpResponse:
     contex = {
-        'title': 'blog page'
+        'title': 'blog page',
+        'blogs': Blog.objects.all()
     }
     return render(request,
                   'blog/templates/blog.html',
