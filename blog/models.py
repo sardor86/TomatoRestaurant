a271c087e45e956c5239cde253f4c441352eb93b
sardor <sshavkatov900@gmail.com>
2024-01-12 21:23:13 +0500

    added blog comment models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from shop.models import User
+
 
 class Blog(models.Model):
     image = models.ImageField(upload_to='blog')
@@ -18,3 +20,20 @@ class Blog(models.Model):
 
     def __repr__(self) -> str:
         return f'Blog<{self.title}>'
+
+
+class BlogComment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=250)
+
+    class Meta:
+        db_table = 'blog_comment'
+        verbose_name = 'comment'
+        verbose_name_plural = 'blog comments'
+
+    def __str__(self) -> str:
+        return f'BlogComment<{self.comment}>'
+
+    def __repr__(self) -> str:
+        return f'Comment<{self.comment}>'
